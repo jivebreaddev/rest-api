d6025198e13437fb3559413bc4d42422184f479f
Jivebreaddev <jivebreaddev@gmail.com>
2022-06-06 22:19:26 +0900

    authentication user model added

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2Error
 
-from django.core.management import call_commnad
+from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
@@ -20,8 +20,8 @@ class CommandTest(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=["default"])
-        
+        patched_check.assert_called_once_with(databases=["default"])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operational ERROR"""
@@ -30,5 +30,5 @@ class CommandTest(SimpleTestCase):
         call_command['wait_for_db']
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
     
